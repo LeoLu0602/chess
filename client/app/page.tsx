74995268d28f6682a7719113ef7ccf1ab02a7391
Leo Lu <lyuholu@gmail.com>
2025-06-03 12:46:25 +0800

    feat: add board

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,11 @@
+import Board from '@/components/Board';
+
 export default function Home() {
-    return <>Home</>;
+    return (
+        <main className="fixed left-0 top-0 w-screen h-screen flex justify-center items-center">
+            <div className="h-[464px] w-[464px]">
+                <Board />
+            </div>
+        </main>
+    );
 }
---
+++ client/components/Board.tsx
@@ -0,0 +1,3 @@
+export default function Board() {
+    return <div className="w-full h-full bg-emerald-500"></div>;
+}
