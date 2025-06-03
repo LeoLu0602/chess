611fdd1a3332e9dde2d201f03783f2676eb779a3
Leo Lu <lyuholu@gmail.com>
2025-06-03 13:26:10 +0800

    chore: move game to /play

--- client/app/play/page.tsx
+++ client/app/play/page.tsx
@@ -1,6 +1,6 @@
 import Board from '@/components/Board';
 
-export default function Home() {
+export default function Play() {
     return (
         <main className="fixed left-0 top-0 w-screen h-screen flex justify-center items-center bg-[#302e2b]">
             <div className="h-[464px] w-[464px]">
