a49faa93875c24a20f714ab19260ee7c4d111970
Leo Lu <lyuholu@gmail.com>
2025-06-04 13:13:29 +0800

    test: add a test for rook

--- client/__test__/utils.test.ts
+++ client/__test__/utils.test.ts
@@ -2,19 +2,25 @@ import '@testing-library/jest-dom';
 import { getLegalMoves } from '@/utils';
 import { CellType } from '@/types';
 
-describe('', () => {
-    it('', () => {
+describe('getLegalMoves', () => {
+    it('should get legal moves for rook on an empty board', () => {
         const board: CellType[][] = [
-            ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br'],
-            ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', 'wr', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
-            ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
-            ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr'],
+        ];
+        const actual: number[] = getLegalMoves(board, 0, 0, 'w');
+        const expected: number[] = [
+            3, 11, 19, 35, 43, 51, 59, 24, 25, 26, 28, 29, 30, 31,
         ];
 
-        expect(getLegalMoves(board, 0, 0, 'w')).toEqual([]);
+        expect(actual.sort((a, b) => a - b)).toEqual(
+            expected.sort((a, b) => a - b)
+        );
     });
 });
