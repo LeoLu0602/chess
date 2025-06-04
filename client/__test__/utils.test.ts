f6483a9ced8105721d0d74fe6350d232f3c829a6
Leo Lu <lyuholu@gmail.com>
2025-06-04 13:18:01 +0800

    test: add a test for bishop

--- client/__test__/utils.test.ts
+++ client/__test__/utils.test.ts
@@ -3,7 +3,7 @@ import { getLegalMoves } from '@/utils';
 import { CellType } from '@/types';
 
 describe('getLegalMoves', () => {
-    it('should get legal moves for rook on an empty board', () => {
+    test('get legal moves for rook on an empty board', () => {
         const board: CellType[][] = [
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
@@ -14,9 +14,30 @@ describe('getLegalMoves', () => {
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
         ];
-        const actual: number[] = getLegalMoves(board, 0, 0, 'w');
+        const actual: number[] = getLegalMoves(board, 3, 3, 'w');
         const expected: number[] = [
             3, 11, 19, 35, 43, 51, 59, 24, 25, 26, 28, 29, 30, 31,
+        ];
+
+        expect(actual.sort((a, b) => a - b)).toEqual(
+            expected.sort((a, b) => a - b)
+        );
+    });
+
+    test('get legal moves for bishop on an empty board', () => {
+        const board: CellType[][] = [
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', 'wb', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+        ];
+        const actual: number[] = getLegalMoves(board, 3, 3, 'w');
+        const expected: number[] = [
+            0, 9, 18, 36, 45, 54, 63, 48, 41, 34, 20, 13, 6,
         ];
 
         expect(actual.sort((a, b) => a - b)).toEqual(
